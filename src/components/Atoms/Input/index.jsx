d3a714b5a81356cdb3f52997ebb204572dda9afc
mkazu0428 <k.miyahara@4nonome.com>
2023-03-06 17:08:00 +0900

    #Atoms/Inputの作成2

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+import FONTFAMILY from "../../../variables/font_family";
+
+const Input = ({ defaultValue = "", onEditComplete }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.value = defaultValue;
+    inputRef.current.focus();
+    inputRef.current.addEventListener(
+      "focusout",
+      onEditComplete(inputRef.current.value)
+    );
+    inputRef.current.addEventListener("keydown", (e) => {
+      if (e.keyCode === 13) {
+        inputRef.current.removeEventListener("focusout", onEditComplete());
+        onEditComplete(inputRef.current.value);
+      }
+    });
+  }, []);
+
+  return <InputTag ref={inputRef} type="text"></InputTag>;
+};
+
+export default Input;
+
+const InputTag = styled.input`
+  width: 100%;
+  height: 20px;
+  border: none;
+  border-radius: 2px;
+  background-color: ${COLOR.BLACK};
+  ${TEXT.S}
+  color:white;
+  font-family: ${FONTFAMILY.NOTO_SANS};
+
+  &:focus {
+    outline: 0;
+    caret-color: ${COLOR.LIGHT_GRAY};
+  }
+`;
