236a2f0627c3c06b58cff7fc97786a9088d93d02
mkazu0428 <k.miyahara@4nonome.com>
2023-03-15 15:23:25 +0900

    first commit

---
+++ src/components/Pages/MainPage/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+import Title from "../../Atoms/Title";
+//import TodoCard from "../../Orgasnisms/TodoCard"
+
+const MainPage = () => {};
+
+export default MainPage;
