a358e847500adbd3437be85deaf8263e73d27fc5
mkazu0428 <k.miyahara@4nonome.com>
2023-03-19 18:25:47 +0900

    first commit

---
+++ src/components/Pages/MainPage/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import Title from "../../Atoms/Title";
+import TodoCard from "../../Organisms/TodoCard";
+import COLOR from "../../../variables/color";
+
+const MainPage = () => {
+  return (
+    <StyledWrapper>
+      <Title />
+      <TodoCard />
+    </StyledWrapper>
+  );
+};
+
+export default MainPage;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background-color: ${COLOR.DEEP_BLUE_GRAY};
+`;
